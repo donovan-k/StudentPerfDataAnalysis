4e1ef10aa14dfd58a8987a6567ba0d602f731a18
donov <dkitten2@illinois.edu>
2020-12-02 16:24:32 -0600

    Fixed test python file for testing StatCalculation

--- test_stats.py
+++ test_stats.py
@@ -1,6 +1,6 @@
-# Testing file for statistical_measurement Subject algorithms
+# Testing file for StatCalculation calculations
 # libraries
-import statistical_measurements
+import StatCalculation as sC
 import pandas as pd
 import unittest
 
@@ -9,16 +9,16 @@ class TestStatMeasurements(unittest.TestCase):
 
     def test_sum(self):
         df = pd.read_csv('test_file.csv')
-        sub = statistical_measurements.Subject(df['numbers'])
+        sub = sC.StatCalc(df['numbers'])
         self.assertEqual(sub.sum_of(), 15)
 
     def test_mean(self):
         df = pd.read_csv('test_file.csv')
-        sub = statistical_measurements.Subject(df['numbers'])
+        sub = sC.StatCalc(df['numbers'])
         self.assertEqual(sub.mean_of(), 3)
 
     def test_var(self):
         df = pd.read_csv('test_file.csv')
-        sub = statistical_measurements.Subject(df['numbers'])
+        sub = sC.StatCalc(df['numbers'])
         self.assertEqual(sub.var_of(), 2.5)
 
